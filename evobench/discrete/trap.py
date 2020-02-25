e950ccca236d1783fa0e1754ce43598b6ea39752
Piotr Rarus <piotr.rarus@softwarehut.com>
2020-02-25 14:18:59 +0100

    Added blocks definition to separable.py

--- evobench/discrete/trap.py
+++ evobench/discrete/trap.py
@@ -1,22 +1,18 @@
 import numpy as np
+from typing import List
 
 from evobench.separable import Separable
 
 
 class Trap(Separable):
 
-    def __init__(
-        self,
-        block_size: int,
-        repetitions: int,
-        overlap_size: int = 0
-    ):
-        super(Trap, self).__init__(block_size, repetitions, overlap_size)
+    def __init__(self, blocks: List[int], overlap_size: int = 0):
+        super(Trap, self).__init__(blocks, overlap_size)
 
-        self.GLOBAL_OPTIMUM = self.BLOCK_SIZE
+        self.GLOBAL_OPTIMUM = sum(blocks)
 
     def evaluate_block(self, block: np.ndarray, block_index: int) -> int:
         if not block.any():
-            return self.GLOBAL_OPTIMUM
+            return block.size
         else:
             return np.count_nonzero(block) - 1
--- evobench/separable.py
+++ evobench/separable.py
@@ -1,5 +1,5 @@
 from abc import abstractmethod
-from typing import Dict
+from typing import Dict, List
 
 import numpy as np
 from lazy import lazy
@@ -14,32 +14,22 @@ class Separable(Benchmark):
     Base class for fully separable problems.
     """
 
-    def __init__(
-        self,
-        block_size: int,
-        repetitions: int,
-        overlap_size: int = 0
-    ):
+    def __init__(self, blocks: List[int], overlap_size: int = 0):
         """
         Parameters
         ----------
-        block_size : int
-            Size of a single block.
-        repetitions : int
-            Number of concats.
-            That many times your base problem will be repeated.
+        blocks : int
+            Sizes of each block
         overlap_size : int, optional
             That many genes will overlap between different blocks, by default 0
         """
 
         super(Separable, self).__init__()
 
-        self.BLOCK_SIZE = block_size
-        self.REPETITIONS = repetitions
-        self.OVERLAP_SIZE = overlap_size
+        self.BLOCKS = blocks
 
-        self.GENOME_SIZE = block_size * repetitions
-        self.GENOME_SIZE -= (repetitions - 1) * self.OVERLAP_SIZE
+        self.GENOME_SIZE = sum(blocks)
+        self.GENOME_SIZE -= (len(blocks) - 1) * self.OVERLAP_SIZE
 
     @lazy
     def genome_size(self) -> int:
@@ -53,9 +43,7 @@ class Separable(Benchmark):
         """
 
         as_dict = {}
-        as_dict['block_size'] = self.BLOCK_SIZE
-        as_dict['repetitions'] = self.REPETITIONS
-        as_dict['overlap_size'] = self.OVERLAP_SIZE
+        as_dict['blocks'] = self.BLOCKS
 
         benchmark_as_dict = super().as_dict
         as_dict = {**benchmark_as_dict, **as_dict}
@@ -65,11 +53,14 @@ class Separable(Benchmark):
     def _evaluate_solution(self, solution: Solution) -> float:
 
         blocks = []
+        start = 0
 
-        for r in range(self.REPETITIONS):
-            start = r * self.BLOCK_SIZE - r * self.OVERLAP_SIZE
-            block = solution.genome[start:start+self.BLOCK_SIZE]
+        for index, block_size in enumerate(self.BLOCKS):
+
+            block = solution.genome[start: start + block_size]
             blocks.append(block)
+
+            start += block_size - index * self.OVERLAP_SIZE
 
         score = sum(
             self.evaluate_block(block, index)
