55f3918d439d88dbd112ffafea42a0b9d5aeb470
Piotr Rarus <piotr.rarus@softwarehut.com>
2020-04-02 10:23:36 +0200

    Added hash to solution

--- evobench/model/solution.py
+++ evobench/model/solution.py
@@ -1,8 +1,13 @@
 from dataclasses import dataclass
-
+from lazy import lazy
 from numpy import ndarray
+from hashlib import blake2b
 
 
 @dataclass(frozen=True)
 class Solution:
     genome: ndarray
+
+    @lazy
+    def __hash__(self):
+        return blake2b(self.genome.tobytes()).hexdigest()
