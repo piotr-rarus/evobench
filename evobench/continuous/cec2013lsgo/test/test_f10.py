569db4aa40282bef0618a84f142c50079c657838
Piotr Rarus <piotr.rarus@gmail.com>
2021-12-13 12:03:32 +0100

    Adds F10

---
+++ evobench/continuous/cec2013lsgo/test/test_f10.py
@@ -0,0 +1,20 @@
+from pytest import fixture
+
+from ..f10 import F10
+
+
+@fixture(scope="module")
+def f10() -> F10:
+    return F10()
+
+
+def test_data_files(f10: F10, helpers):
+    helpers.test_data_files(f10)
+
+
+def test_evaluate_solution(f10: F10, helpers):
+    helpers.test_evaluate_solution(f10)
+
+
+def test_evaluate_population(f10: F10, helpers):
+    helpers.test_evaluate_population(f10)
