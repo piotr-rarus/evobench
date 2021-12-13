dff61a4ddb9554a9fa67f8ccd8e20ae1c1d851c8
Piotr Rarus <piotr.rarus@gmail.com>
2021-12-13 12:50:58 +0100

    Adds F13

---
+++ evobench/continuous/cec2013lsgo/test/test_f13.py
@@ -0,0 +1,20 @@
+from pytest import fixture
+
+from ..f13 import F13
+
+
+@fixture(scope="module")
+def f13() -> F13:
+    return F13()
+
+
+def test_data_files(f13: F13, helpers):
+    helpers.test_data_files(f13)
+
+
+def test_evaluate_solution(f13: F13, helpers):
+    helpers.test_evaluate_solution(f13)
+
+
+def test_evaluate_population(f13: F13, helpers):
+    helpers.test_evaluate_population(f13)
